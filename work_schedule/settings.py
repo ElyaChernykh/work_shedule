987cde15dcdf075966619cb8b70ad54b5f3cbc3e
ElyaChernykh <elvira.116.elf@gmail.com>
2026-06-19 02:47:01 +0300

    Исправлен ALLOWED_HOSTS для Render

--- work_schedule/settings.py
+++ work_schedule/settings.py
@@ -28,7 +28,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ['work-shedule.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
